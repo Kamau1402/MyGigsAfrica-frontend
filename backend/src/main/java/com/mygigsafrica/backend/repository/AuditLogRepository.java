73856be980e103f5609206679befd610f99669ef
Kamau1402 <kamauvincent23@gmail.com>
2025-07-28 13:29:15 +0300

    Remove unwanted files/folders

--- backend/src/main/java/com/mygigsafrica/backend/repository/AuditLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mygigsafrica.backend.repository;
-
-import com.mygigsafrica.backend.entity.AuditLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
-    List<AuditLog> findByUserId(Long userId);
-}
